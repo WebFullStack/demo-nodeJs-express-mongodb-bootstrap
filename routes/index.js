498f209c076c76148464c2aef3e25dd35f9da7f8
hehaiyangwork <hehaiyangwork@qq.com>
2016-09-21 13:54:56 +0800

    Added: bootstrap js and css

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET index page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET console page. */
+router.get('/console', function(req, res, next) {
+  res.render('console', { title: 'Express' });
 });
 
 module.exports = router;
